2cce6d8c7948808aa5160e91ace5f663b727a711
Umang Rathod <75923134+umang00775@users.noreply.github.com>
2022-09-24 16:28:25 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const https = require('https');
+const session = require('express-session');
+const passport = require('passport');
+const bcrypt = require('bcrypt');
+const app = express();
+const port = 3000 || process.env.PORT;
+
+// Setting :)
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended:true}));
+
+/* From disk */
+const data = require('./javascript/data.js');
+
+app.get('/',(req,res)=>{
+    // res.sendFile(__dirname + '/static//index.html');
+    res.render('home',{data:data});
+});
+
+app.listen(port,()=>{
+    console.log(`Listening to port ${port}`);
+});
+
+app.get('/jobs', (req,res)=>{
+    res.render('jobSearch');
+});
+
+
+app.get('/certificate', (req,res)=>{
+    const date = new Date().getDate();
+    const month = new Date().getMonth();
+    const year = new Date().getFullYear();
+    const fullDate = date + "/" + month + "/" + year;
+    const certData = {
+        goodName:"Umang Rathod",
+        courseName: "Full stack web development",
+        date: fullDate,
+    }
+    res.render('certificateGenerator', {certData: certData});
+});
+
+
+app.get('/courses', (req,res)=>{
+    res.render('courses');
+});
+
+app.get('/loan', (req,res)=>{
+    res.render('loanApplication');
+});
+
+
+app.get('/login', (req,res)=>{
+    res.render('login');
+});
---
+++ javascript/data.js
@@ -0,0 +1,42 @@
+module.exports = {
+    webAppName:"Queen Site",
+    courses:{
+        it:[
+            {
+                imgLink:"",
+                link:"https://youtu.be/eKqY-oP1d_Y",
+                instructor:"Shraddha Khapra",
+                description:"Are you worried about placements/internships? Want to prepare for companies like Microsoft, Amazon & Google? Join the course.",
+                completed:false,
+            },
+            {
+                imgLink:"",
+                link:"https://youtu.be/vLqTf2b6GZw",
+                instructor:"Aman Dhattarval and Shraddha Khapra",
+                description:"This video shows installation of 2 Python editors, both are free to use. For window users I have downloaded PyScripter in this video.",
+                completed:false,
+            },
+            {
+                imgLink:"",
+                link:"https://youtu.be/20v8zSo2v18",
+                instructor:"Sumit Malik",
+                description:"Are you worried about placements/internships? Want to prepare for companies like Microsoft, Amazon & Google? Join the course.",
+                completed:false,
+            },
+            {
+                imgLink:"",
+                link:"https://youtu.be/_k_c9nqzKN0",
+                instructor:"Aditya Verma",
+                description:"Are you worried about placements/internships? Want to prepare for companies like Microsoft, Amazon & Google? Join the course.",
+                completed:false,
+            },
+            {
+                imgLink:"",
+                link:"https://youtu.be/UmnCZ7-9yDY",
+                instructor:"Aman Dhattarval",
+                description:"Are you worried about placements/internships? Want to prepare for companies like Microsoft, Amazon & Google? Join the course.",
+                completed:false,
+            },
+        ]
+    }
+}
